8e1f92f840ac1e5a451f5a7367fe7fafe651fcb4
asyljanov <43650395+asyljanov@users.noreply.github.com>
2018-10-22 22:30:02 +0600

    Update mathdo.c

--- MATHDO/mathdo.c
+++ MATHDO/mathdo.c
@@ -2,22 +2,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
-int main(int argc, char* argv[]){
-	int pid = fork(); // child1
-	if (pid==0){ // child 1
-		execlp("./add.py",":)",argv[1],argv[2],NULL);
-
-	} else // parent
-	{
-		int pid2 = fork();
-		if (pid2==0){ // child 2
-			execlp("./div.sh",":)",argv[1],argv[2],NULL);
-		} else // parent
-		{
-			wait(0);
-			wait(0);
-			printf("parent: done\n");
-		}		
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[]){
+	int p1 = fork();
+	if(p1==0){
+		printf("I am child of %d with pid %d\n",(int)getppid(),(int)getpid());
+		execlp("python", "python","addition.py",argv[1],argv[2],NULL);
 	}
+	else{
+		int p2 = fork();
+		if(p2==0){
+			printf("I am child of %d with pid %d\n",(int)getppid(),(int)getpid());
+			execlp("java", "java","Subtraction",argv[1],argv[2],NULL);
+		}
+		else{
+			int p3 = fork();
+			if(p3==0){
+				printf("I am child of %d with pid %d\n",(int)getppid(),(int)getpid());
+				execlp("node", "node","multiplication.js",argv[1],argv[2],NULL);
+			}
+			else{
+				int p4 = fork();
+				if(p4==0){
+					printf("I am child of %d with pid %d\n",(int)getppid(),(int)getpid());
+					execlp("bash", "bash","division.sh",argv[1],argv[2],NULL);
+				}
+				else{
+					wait(0);
+					wait(0);
+					wait(0);
+					wait(0);
+					printf("parent: done.\n");
+				}
+			}
+		}
+	}
+	return 0;
 }
